2de5d9bda0f3474ae5901aebccce313886052693
JohnnyBjerring <83641312+JohnnyBjerring@users.noreply.github.com>
2025-08-14 15:04:41 +0200

    Models added
    Movie and genre models added

---
+++ The Movies/TheMovies/TheMovies/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Models
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public int? Lenght { get; set; }
+
+    }
+}
