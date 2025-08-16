d6b0889c7f8ee4f7ef897703dd87efa65349e1fe
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-08-16 23:34:39 +0200

    Added main View, with buttons etc.
    Missing Bindings and code behind

---
+++ The Movies/TheMovies/TheMovies/View/MainPageView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TheMovies.ViewModels;
+
+namespace TheMovies.View
+{
+    public partial class MainPageView : Page
+    {
+
+        private Frame _frame;
+        private MainViewModel _mainViewModel;
+
+        public MainPageView(Frame frame)
+        {
+            InitializeComponent();
+            _frame = frame;
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+        }
+
+        private void addMovie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeMovie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void placeholder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
