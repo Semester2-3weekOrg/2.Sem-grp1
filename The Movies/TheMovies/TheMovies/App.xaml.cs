608d191c6a9dc3e9b8adbd2af53a09cc07dc1b42
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-08-21 12:40:34 +0200

    Removed a hidden "private" that caused the project not to run

--- The Movies/TheMovies/TheMovies/App.xaml.cs
+++ The Movies/TheMovies/TheMovies/App.xaml.cs
@@ -2,13 +2,15 @@
 
 namespace TheMovies
 {
-    private 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 
 }
