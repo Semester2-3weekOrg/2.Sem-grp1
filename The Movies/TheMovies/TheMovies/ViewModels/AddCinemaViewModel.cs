f84a2fe14a534e9d42a21d8fd2bd5fa156f30082
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-08-26 10:19:05 +0200

    Refactor view models to inherit from ViewModelBase
    The `AddCinemaHallViewModel` and `AddCinemaViewModel` classes have been updated to inherit from `ViewModelBase`, eliminating the need for direct implementation of `INotifyPropertyChanged`. This change simplifies the code by removing unnecessary event handling and related `using` directives. Additionally, regions associated with `INotifyPropertyChanged` have been removed, enhancing code readability.

--- The Movies/TheMovies/TheMovies/ViewModels/AddCinemaViewModel.cs
+++ The Movies/TheMovies/TheMovies/ViewModels/AddCinemaViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using TheMovies.Commands;
